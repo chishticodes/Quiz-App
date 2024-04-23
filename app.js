40ab1973a82edf690d437c420dcf4fc8cd69391b
chishticodes <chishticodes@gmail.com>
2024-04-24 01:49:46 +0500

    Question and option for quiz updated

---
+++ app.js
@@ -0,0 +1,38 @@
+const questions = [
+    {
+        question: "Which is the largest animal on the planet?",
+        answers:[
+            { text: "Shark", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false }
+        ]
+    },
+    {
+        question: " What is the biggest County in terms of Area?",
+        answers:[
+            { text: "Russia", correct: true },
+            { text: "America", correct: false },
+            { text: "Canada", correct: false },
+            { text: "China", correct: false }
+        ]
+    },
+    {
+        question: "What is the most Spoken Language in the World?",
+        answers:[
+            { text: "English", correct: false },
+            { text: "Spanish", correct: false },
+            { text: "Mandarin", correct: true },
+            { text: "Arabic", correct: false }
+        ]
+    },
+    {
+        question: "What is the largest River in the World?",
+        answers:[
+            { text: "Amazon", correct: false },
+            { text: "Nile", correct: false },
+            { text: "Thames", correct: true },
+            { text: "Indus", correct: false }
+        ]
+    }
+];
